7da9b230187bf7a1f5e5d540564da7082caaedb6
candy197 <candy.jacob1997@gmail.com>
2024-01-20 19:58:20 +0530

    changes

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import yfinance as yf
+from nsetools import Nse
+import nsepy as Np
+st.title(':rainbow[Welcome To Trading World] :blue[Be Cool Trader] :sunglasses:')
+st.header('Letz be billionaire Together', divider='rainbow')
---
+++ pages/Equity.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import yfinance as yf
+from nsetools import Nse
+import nsepy as Np
+st.title(':rainbow[Welcome To Trading World] :blue[Be Cool Trader] :sunglasses:')
+st.header('Letz be billionaire Together', divider='rainbow')
+
+#Fetch like (Low, High,Priviously Closed,R)
+
+
+
+
+
