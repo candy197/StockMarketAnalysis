c2190bcdee77ed83417760a97f422353c3744568
candy197 <candy.jacob1997@gmail.com>
2024-01-20 20:01:58 +0530

    newly added

---
+++ pages/MF.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import yfinance as yf
+from nsetools import Nse
+import nsepy as Np
+st.title(':rainbow[Welcome To Mutual Fund Analysis] :blue[Be Cool Trader] :sunglasses:')
+st.header('Letz be Long term player in market', divider='rainbow')
---
+++ pages/Options.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import yfinance as yf
+from nsetools import Nse
+import nsepy as Np
+st.title(':rainbow[Welcome To Opetions Trading World] :blue[Be Cool Trader] :sunglasses:')
+st.header('Be cautions while investing into Future and Options', divider='rainbow')
